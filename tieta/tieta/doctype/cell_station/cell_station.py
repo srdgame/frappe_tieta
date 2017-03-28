a0ccd162ea904b735316492a6926fd4965b0f24b
Dirk Chang <dirk@kooiot.com>
2017-03-28 17:18:10 +0800

    Auto Create/Delete Cloud Project Site

--- tieta/tieta/doctype/cell_station/cell_station.py
+++ tieta/tieta/doctype/cell_station/cell_station.py
@@ -11,3 +11,23 @@ class CellStation(Document):
 		self.province_name = frappe.get_value("Region", self.province, "region_name")
 		self.city_name = frappe.get_value("Region", self.city, "region_name")
 		self.town_name = frappe.get_value("Region", self.town, "region_name")
+
+	def after_insert(self):
+		data = {
+			"naming_series": "CELL-",
+			"project": self.project,
+			"site_name": self.station_name,
+			"address": self.address,
+			"longitude": self.longitude,
+			"latitude": self.latitude,
+		}
+		data.update({
+			"doctype": "Cloud Project Site"
+		})
+		doc = frappe.get_doc(data)
+		doc = doc.insert(ignore_permissions=True)
+		self.site = doc.name
+		self.save()
+
+	def on_trash(self):
+		frappe.delete_doc("Cloud Project Site", self.site, ignore_permissions=True)
