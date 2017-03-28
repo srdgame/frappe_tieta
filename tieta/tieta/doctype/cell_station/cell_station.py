57006d8ae6df5fe69bd5973dc44efd3e5c58c0ee
Dirk Chang <dirk@kooiot.com>
2017-03-28 17:08:23 +0800

    Show region name

--- tieta/tieta/doctype/cell_station/cell_station.py
+++ tieta/tieta/doctype/cell_station/cell_station.py
@@ -7,4 +7,7 @@ import frappe
 from frappe.model.document import Document
 
 class CellStation(Document):
-	pass
+	def validate(self):
+		self.province_name = frappe.get_value("Region", self.province, "region_name")
+		self.city_name = frappe.get_value("Region", self.city, "region_name")
+		self.town_name = frappe.get_value("Region", self.town, "region_name")
