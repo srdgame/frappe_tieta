f4d61958f986dfa6724a042fd859152ebd382542
Dirk Chang <dirk@kooiot.com>
2017-03-28 16:56:58 +0800

    query region

--- tieta/tieta/doctype/cell_station/cell_station.js
+++ tieta/tieta/doctype/cell_station/cell_station.js
@@ -2,6 +2,25 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Cell Station', {
+	setup: function(frm) {
+		frm.fields_dict['province'].get_query  = function(){
+			return {
+				query:"tieta.tieta.doctype.region.region.query_province",
+			};
+		};
+		frm.fields_dict['city'].get_query  = function(){
+			return {
+				query:"tieta.tieta.doctype.region.region.query_city",
+				filters: {"province": frm.doc.province}
+			};
+		};
+		frm.fields_dict['town'].get_query  = function(){
+			return {
+				query:"tieta.tieta.doctype.region.region.query_town",
+				filters: {"city": frm.doc.city}
+			};
+		};
+	},
 	refresh: function(frm) {
 
 	}
--- tieta/tieta/doctype/region/region.py
+++ tieta/tieta/doctype/region/region.py
@@ -8,3 +8,27 @@ from frappe.model.document import Document
 
 class Region(Document):
 	pass
+
+
+def query_province(doctype, txt, searchfield, start, page_len, filters):
+	return frappe.db.sql("""select name, description from `tabRegion`
+		where type = 'Province'
+		and %s like %s order by name limit %s, %s""" %
+		(searchfield, "%s", "%s", "%s"),
+		("%%%s%%" % txt, start, page_len), as_list=1)
+
+
+def query_city(doctype, txt, searchfield, start, page_len, filters):
+	return frappe.db.sql("""select name, description from `tabRegion`
+		where type = 'City' and region_parent = %s
+		and %s like %s order by name limit %s, %s""" %
+		("%s", searchfield, "%s", "%s", "%s"),
+		(filters["province"], "%%%s%%" % txt, start, page_len), as_list=1)
+
+
+def query_town(doctype, txt, searchfield, start, page_len, filters):
+	return frappe.db.sql("""select name, description from `tabRegion`
+		where type = 'Town' and region_parent = %s
+		and %s like %s order by name limit %s, %s""" %
+		(searchfield, "%s", "%s", "%s"),
+		(filters["city"], "%%%s%%" % txt, start, page_len), as_list=1)
