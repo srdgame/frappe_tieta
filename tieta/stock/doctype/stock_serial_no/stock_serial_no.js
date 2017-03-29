f297aa71643df0566c210a75691fbcd554ae64e9
Dirk Chang <dirk@kooiot.com>
2017-03-29 18:34:25 +0800

    filter item attributes

--- tieta/stock/doctype/stock_serial_no/stock_serial_no.js
+++ tieta/stock/doctype/stock_serial_no/stock_serial_no.js
@@ -13,6 +13,25 @@ frappe.ui.form.on('Stock Serial No', {
 		};
 	},
 	refresh: function(frm) {
-
+		if (ss_item_code != frm.doc.item_code) {
+			ss_item_code = frm.doc.item_code;
+			frappe.call({
+				method: 'tieta.stock.doctype.stock_item_attribute.stock_item_attribute.stock_item_attribute_query',
+				args: {
+					"type": "serial",
+					"item_code": ss_item_code
+				},
+				callback: function (r) {
+					frm.set_value("attributes" ,"");
+					if (r.message) {
+						$.each(r.message, function(i, d) {
+							var row = frappe.model.add_child(cur_frm.doc, "Stock Batch NoAttribute", "attributes");
+							row.attribute = d.attribute;
+						});
+					}
+					refresh_field("attributes");
+				}
+			});
+		}
 	}
 });
