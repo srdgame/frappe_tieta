c5a49ed7bd8241e54971f4f1b58ab88e1d26065e
Dirk Chang <dirk@kooiot.com>
2017-03-29 19:02:54 +0800

    filter item attributes

--- tieta/stock/doctype/stock_serial_no/stock_serial_no.js
+++ tieta/stock/doctype/stock_serial_no/stock_serial_no.js
@@ -18,24 +18,22 @@ frappe.ui.form.on('Stock Serial No', {
 });
 
 frappe.ui.form.on("Stock Serial No", "item_code", function(frm) {
-	args = {
-		"doctype": "Stock Item Attribute",
-		"txt": "",
-		"filters": {
-			"type": "serial",
-			"item_code": frm.doc.item_code
-		}
-	};
-	alert(args);
-
 	frappe.call({
 		type: "GET",
 		method:'frappe.desk.search.search_link',
-		args: args,
+		args: {
+			"doctype": "Stock Item Attribute",
+			"txt": "",
+			"filters": {
+				"type": "serial",
+				"item_code": frm.doc.item_code
+			}
+		},
 		callback: function (r) {
 			frm.set_value("attributes" ,"");
 			if (r.message) {
 				$.each(r.message, function(i, d) {
+					alert( d.attribute);
 					var row = frappe.model.add_child(cur_frm.doc, "Stock Batch NoAttribute", "attributes");
 					row.attribute = d.attribute;
 				});
