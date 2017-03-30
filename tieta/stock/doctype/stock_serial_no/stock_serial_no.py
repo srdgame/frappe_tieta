e4f9f139e66167b2a5e7d42199101431be136aef
Dirk Chang <dirk@kooiot.com>
2017-03-30 17:44:20 +0800

    query functions

--- tieta/stock/doctype/stock_serial_no/stock_serial_no.py
+++ tieta/stock/doctype/stock_serial_no/stock_serial_no.py
@@ -16,7 +16,7 @@ def stock_serial_no_query(doctype, txt, searchfield, start, page_len, filters):
 
 	item_code = filters["item_code"]
 	return frappe.db.sql("""select name, warehouse from `tabStock Serial No`
-		where item_code = %s and docstatus = 2
+		where item_code = %s and docstatus = 1
 		and %s like %s order by name limit %s, %s""" %
 		("%s", searchfield, "%s", "%s", "%s"),
 		(item_code, "%%%s%%" % txt, start, page_len), as_list=1)
