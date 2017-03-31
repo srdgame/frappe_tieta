19939f25caa12d97ca88b56efd63f74eb3d8b603
Dirk Chang <dirk.chang@symid.com>
2017-03-30 22:55:29 -0700

    show the custom button

--- tieta/tieta/doctype/cell_station/cell_station.js
+++ tieta/tieta/doctype/cell_station/cell_station.js
@@ -29,10 +29,7 @@ frappe.ui.form.on('Cell Station', {
 	},
 	refresh: function (frm) {
 		var grid = frm.fields_dict["devices"].grid;
-		grid.set_column_disp("device_type_value", true);
-
-		/*
-		grid.add_items_button = grid.add_custom_button(__('Add Device Items'), function() {
+		grid.add_custom_button(__('Add Device Items'), function() {
 			frappe.call({
 				type: "GET",
 				method: 'frappe.desk.search.search_link',
@@ -57,7 +54,6 @@ frappe.ui.form.on('Cell Station', {
 				}
 			});
 		});
-		*/
 	},
 });
 
