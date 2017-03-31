ff1d779fc2e13c6c180c4d830d7c339ef71a11c5
Dirk Chang <dirk@kooiot.com>
2017-03-31 11:26:33 +0800

    Auto fetch the device type

--- tieta/tieta/doctype/cell_station/cell_station.js
+++ tieta/tieta/doctype/cell_station/cell_station.js
@@ -2,35 +2,38 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Cell Station', {
-	setup: function(frm) {
-		frm.fields_dict['province'].get_query  = function(){
+	setup: function (frm) {
+		frm.fields_dict['province'].get_query = function () {
 			return {
-				query:"tieta.tieta.doctype.region.region.query_province",
+				query: "tieta.tieta.doctype.region.region.query_province",
 			};
 		};
-		frm.fields_dict['city'].get_query  = function(){
+		frm.fields_dict['city'].get_query = function () {
 			return {
-				query:"tieta.tieta.doctype.region.region.query_city",
+				query: "tieta.tieta.doctype.region.region.query_city",
 				filters: {"province": frm.doc.province}
 			};
 		};
-		frm.fields_dict['county'].get_query  = function(){
+		frm.fields_dict['county'].get_query = function () {
 			return {
-				query:"tieta.tieta.doctype.region.region.query_county",
+				query: "tieta.tieta.doctype.region.region.query_county",
 				filters: {"city": frm.doc.city}
 			};
 		};
-		frm.fields_dict['town'].get_query  = function(){
+		frm.fields_dict['town'].get_query = function () {
 			return {
-				query:"tieta.tieta.doctype.region.region.query_town",
+				query: "tieta.tieta.doctype.region.region.query_town",
 				filters: {"county": frm.doc.county}
 			};
 		};
 	},
-	refresh: function(frm) {
+	refresh: function (frm) {
 
 	},
-	devices: function(doc, cdt, cdn) {
+});
+
+frappe.ui.form.on('Devices', {
+	device_type: function(doc, cdt, cdn) {
 		var d = locals[cdt][cdn];
 		frappe.call({
 			method: "frappe.client.get_value",
