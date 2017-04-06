bb8738e7480fe5891443b063cd93c95b564406f4
Dirk Chang <dirk@kooiot.com>
2017-04-06 14:23:50 +0800

    Create todo

--- tieta/stock/doctype/stock_delivery_order/stock_delivery_order.py
+++ tieta/stock/doctype/stock_delivery_order/stock_delivery_order.py
@@ -6,6 +6,9 @@ from __future__ import unicode_literals
 import frappe
 from frappe import throw, _
 from frappe.model.document import Document
+from frappe.desk.form import assign_to
+from frappe.utils.data import format_datetime
+
 
 class StockDeliveryOrder(Document):
 	def on_cancel(self):
@@ -19,3 +22,19 @@ class StockDeliveryOrder(Document):
 		if self.order_source_type == 'Tickets Ticket':
 			doc = frappe.get_doc('Tickets Ticket', self.order_source_id)
 			doc.run_method("on_delivery_order_commit", self)
+			try:
+				print(format_datetime(self.planned_date))
+				desc = _("Stock Delivery Order has been submitted\n Planned Date: {0}").format(
+					format_datetime(self.planned_date)
+				)
+				assign_to.add({
+					'assign_to': self.owner,
+					'doctype': self.doctype,
+					'name': self.name,
+					'description': desc,
+					'priority': 'High',
+					'notify': 1
+				})
+			except assign_to.DuplicateToDoError:
+				frappe.message_log.pop()
+				pass
