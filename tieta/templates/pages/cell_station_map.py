49de521baaa96eea2ce8284c914e138f6fc529fc
Dirk Chang <dirk@kooiot.com>
2017-04-24 19:52:09 +0800

    Redirect to login if user is Guest

--- tieta/templates/pages/cell_station_map.py
+++ tieta/templates/pages/cell_station_map.py
@@ -8,6 +8,9 @@ from frappe import _
 
 
 def get_context(context):
+	if frappe.session.user == 'Guest':
+		frappe.local.flags.redirect_location = "/login"
+		raise frappe.Redirect
 
 	# context.no_cache = 1
 	context.show_sidebar = False
